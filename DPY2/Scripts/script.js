0fdf8dfc0a7d85d3307cd9729483cc4f7bb796f4
john <john@kodalagom.se>
2015-10-14 13:53:14 +0200

    Radial menu

--- DPY2/Scripts/script.js
+++ DPY2/Scripts/script.js
@@ -1,8 +1,22 @@
 ﻿/// <reference path="jquery-2.1.4.js" />
 
+//var count = 0;
+//var wait = setInterval(function () {
+//    $('#cont').append(
+//        $('<div/>')
+//            .addClass('circle')
+//    );
+
+//    count++;
+
+//    if (count == 6) {
+//        clearInterval(wait);
+//    }
+//}, 1000);
+
 $('.circle').click(function () {
     if ($(this).hasClass('explode'))
-        $(this).addClass('implode').removeClass('explode');
+        $(this).removeClass('explode').addClass('implode');
     else
-        $(this).addClass('explode').removeClass('implode');
+        $(this).removeClass('implode').addClass('explode');
 });
