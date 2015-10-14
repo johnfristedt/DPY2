ffe6bbbd6130ae40bbaca84b37a9dbeccfd0f408
john <john@kodalagom.se>
2015-10-14 11:56:58 +0200

    initial commit

---
+++ DPY2/Scripts/script.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="jquery-2.1.4.js" />
+
+$('.circle').click(function () {
+    if ($(this).hasClass('explode'))
+        $(this).addClass('implode').removeClass('explode');
+    else
+        $(this).addClass('explode').removeClass('implode');
+});
